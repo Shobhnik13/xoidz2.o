8fd06d8a6aa4073af3fb010f42d624bfa6a3f783
Shobhnik13 <shobhnikw@gmail.com>
2023-11-29 19:01:36 +0530

    font changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,11 @@ const Navbar = () => {
           {/* for laps  */}
           <div className="sm:hidden lg:block ">
               <div className="flex justify-center ">
-                    <div className="w-[50%] h-[60px] bg-gray-100/100 rounded-2xl py-6  flex justify-between items-center px-4 shadow-xl border mt-4">
+                    <div className="w-[50%] h-[60px]  bg-gray-100/100 rounded-2xl py-6  flex justify-between items-center px-4 shadow-xl border mt-4">
                         {/* left / */}
                         <div className="flex items-center justify-center">
                             <Image alt="logo image" width={120} height={120} src='/xoidz.png'/>
-                            <ul className="pl-3 flex gap-x-4 border-l text-purple-900/100  cursor-pointer text-sm font-medium  border-gray-300">
+                            <ul className="pl-3 flex gap-x-4 border-l text-purple-900/100  cursor-pointer text-sm 2xl:text-lg font-medium  border-gray-300">
                                 <li className=" hover:text-purple-900/80">Product</li>
                                 <li className=" hover:text-purple-900/80">Resources</li>
                                 <li className=" hover:text-purple-900/80">Pricing</li>
@@ -20,7 +20,7 @@ const Navbar = () => {
                         </div>
                         {/* right  */}
                         <div>
-                            <Button variant="secondary" className="bg-white hover:bg-white shadow-md shadow-gray-300 hover:scale-105 text-purple-900/100 ease-in-out transition-all duration-200 rounded-xl">Log in</Button>
+                            <Button variant="secondary" className="bg-white hover:bg-white shadow-md shadow-gray-300 hover:scale-105 text-purple-900/100 ease-in-out transition-all 2xl:text-lg duration-200 rounded-xl">Log in</Button>
                         </div>
                     </div>
               </div>
